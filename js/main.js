46ea6d35b395be70d8fa386db6b96f3b8e74fb08
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-04-26 17:11:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    var swiper = new Swiper(".heroslider", {
+        loop: true,
+        speed: 1000,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                var bulletNumber = (index + 1) < 10 ? '0' + (index + 1) : (index + 1);
+                return '<span class="' + className + '">' + bulletNumber + "</span>";
+            },
+        },
+    });
+
+    // language
+    $('.current__lang').click(function () {
+        $(this).next('ul').slideToggle();
+    });
+
+    // show text
+    $('.showtext').click(function () {
+        $(this).prev('.descr').find('.hidebox').slideToggle();
+        $(this).toggleClass('rotatearr');
+    });
+
+    // testimonials
+    var swiper = new Swiper(".testimslider", {
+        centeredSlides: true,
+        initialSlide: 2,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            640: {
+
+            },
+            768: {
+
+            },
+            1200: {
+                slidesPerView: 2,
+                spaceBetween: 140,
+            },
+        },
+    });
+
+    // header
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 20) {
+            $('.header').addClass('scrollheader');
+        } else {
+            $('.header').removeClass('scrollheader');
+        }
+    });
+
+    if ($(this).scrollTop() > 20) {
+        $('.header').addClass('scrollheader');
+    } else {
+        $('.header').removeClass('scrollheader');
+    }
+
+
+
+
+
+})
