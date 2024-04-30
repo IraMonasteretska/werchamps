11a8999741e12559977dfd437c761ef1693f159c
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-04-30 18:29:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -34,11 +34,13 @@ $(document).ready(function () {
             prevEl: ".swiper-button-prev",
         },
         breakpoints: {
-            640: {
-
-            },
             768: {
-
+                slidesPerView: 1.2,
+                spaceBetween: 50,
+            },
+            992: {
+                slidesPerView: 1.6,
+                spaceBetween: 70,
             },
             1200: {
                 slidesPerView: 2,
@@ -77,5 +79,84 @@ $(document).ready(function () {
         });
     }
 
+    // table content
+    if ($('privacysect__table')) {
+        $(".privacysect__table ul").on("click", "a", function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({ scrollTop: top - 100 }, 1000);
+        });
+
+
+        const links = document.querySelectorAll('.privacysect__table li a');
+        window.addEventListener('scroll', () => {
+            const anchors = document.querySelectorAll('.privacysect__content [id]');
+            anchors.forEach(anchor => {
+                if (anchor.closest('.privacysect__content')) {
+                    const anchorTop = anchor.getBoundingClientRect().top + window.pageYOffset;
+                    if (window.pageYOffset >= anchorTop - 140) {
+                        links.forEach(link => {
+                            const correspondingLink = document.querySelector(`.privacysect__table a[href="#${anchor.id}"]`);
+                            if (correspondingLink) {
+                                link.classList.remove('active');
+                                correspondingLink.classList.add('active');
+                            }
+                        });
+                    }
+                }
+            });
+        });
+
+        $(".infosect__table-content").on("click", function () {
+            $(this).toggleClass('show');
+        });
+    }
+
+    // mob menu
+
+    $('.menubtn').click(function () {
+        $('.mobmenu').addClass('show');
+    });
+
+    $('.closemenu').click(function () {
+        $('.mobmenu').removeClass('show');
+    });
+
+    // ----
+    var menuBtn = document.querySelector('.menubtn');
+    var mobMenu = document.querySelector('.header');
+    var overlay = null;
+
+    menuBtn.addEventListener('click', function () {
+        if (!overlay) {
+            overlay = document.createElement('div');
+            overlay.className = 'overlay';
+            mobMenu.appendChild(overlay);
+            setTimeout(function () {
+                overlay.classList.add('active');
+            }, 10);
+        }
+    });
+
+    var closeMenuBtn = document.querySelector('.closemenu');
+
+    closeMenuBtn.addEventListener('click', closeMenu);
+
+    function closeMenu() {
+        if (overlay) {
+            overlay.classList.remove('active');
+            setTimeout(function () {
+                mobMenu.removeChild(overlay);
+                overlay = null;
+            }, 0);
+        }
+    }
+
+    $('body').on('click', '.overlay', function() {
+        $('.mobmenu').removeClass('show');
+        closeMenu()
+        
+    });
 
 })
